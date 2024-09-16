5011a057d485b6ccbea8c814d1bbd0b81e008b1b
CryingSoul <grebnevegor2000@mail.ru>
2024-09-16 15:54:27 +0300

    Revert "Revert "base struct""
    This reverts commit db58641bc960a66d324c51a1a972ecaaa0c7b4e3.

--- src/managers/ShipManager.cpp
+++ src/managers/ShipManager.cpp
@@ -1,42 +1,69 @@
 #include "ShipManager.hpp"
 #include <iostream>
 
-ShipManager::ShipManager(std::shared_ptr<Field> field) {
-    this->field = field;
+// ????????????? ???? ????? ?????? ?????????????
+ShipManager::ShipManager(std::shared_ptr<Field> gameField)
+    : field(std::move(gameField)), ships() {}
+
+// ??????????? ???????????
+ShipManager::ShipManager(const ShipManager &other)
+    : ships(other.ships), field(other.field) {}
+
+// ???????? ???????????? ???????????
+ShipManager &ShipManager::operator=(const ShipManager &other) {
+  if (this == &other)
+    return *this; // ?????? ?? ????????????????
+  ships = other.ships;
+  field = other.field;
+  return *this;
 }
 
-bool ShipManager::placeShip(std::shared_ptr<Ship> ship, Coordinate cord, bool vertical) {
-    int size = ship->getSize();  
-    
-    ship->printState();  
-        
-    if (vertical) {
-        for (int i = 0; i < size; ++i) {
-            Coordinate newCord = {cord.x + i, cord.y};  
-            if (!field->isValidCoordinate(newCord)) {   
-                std::cerr << "Invalid position for ship placement!" << std::endl;
-                return false;
-            }
-            field->setValueAt(newCord, 'S');  
-        }
-    } else {
-        for (int i = 0; i < size; ++i) {
-            Coordinate newCord = {cord.x, cord.y + i};
-            if (!field->isValidCoordinate(newCord)) {
-                std::cerr << "Invalid position for ship placement!" << std::endl;
-                return false;
-            }
-            field->setValueAt(newCord, 'S');
-        }
+// ??????????? ???????????
+ShipManager::ShipManager(ShipManager &&other) noexcept
+    : ships(std::move(other.ships)), field(std::move(other.field)) {}
+
+// ???????? ???????????? ???????????
+ShipManager &ShipManager::operator=(ShipManager &&other) noexcept {
+  if (this == &other)
+    return *this;
+  ships = std::move(other.ships);
+  field = std::move(other.field);
+  return *this;
+}
+
+bool ShipManager::placeShip(std::shared_ptr<Ship> ship, Coordinate cord,
+                            bool vertical) {
+  int size = ship->getSize();
+  ship->printState();
+
+  if (vertical) {
+    for (uint8_t i = 0; i < size; ++i) {
+      Coordinate newCord = {cord.x + i, cord.y};
+      if (!field->isValidCoordinate(newCord)) {
+        std::cerr << "Invalid position for ship placement!" << std::endl;
+        return false;
+      }
+      field->setValueAt(newCord, CellValue::ShipPart);
     }
-    
-    ships.push_back(ship);  
-    field->display();  
-    return true;
+  } else {
+    for (uint8_t i = 0; i < size; ++i) {
+      Coordinate newCord = {cord.x, cord.y + i};
+      if (!field->isValidCoordinate(newCord)) {
+        std::cerr << "Invalid position for ship placement!" << std::endl;
+        return false;
+      }
+      field->setValueAt(newCord, CellValue::ShipPart);
+    }
+  }
+
+  ships.push_back(ship);
+  system("clear");
+  field->display();
+  return true;
 }
 
 void ShipManager::printAllShips() const {
-    for (const auto& ship : ships) {
-        ship->printState();  
-    }
+  for (const auto &ship : ships) {
+    ship->printState();
+  }
 }
